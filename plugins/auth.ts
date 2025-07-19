9d76d7943b69f82172bb350fbecf5238d8103604
otontas11 <oktay1125@gmail.com>
2025-07-20 00:08:10 +0300

    header menu

--- plugins/auth.ts
+++
@@ -1,9 +0,0 @@
-export default defineNuxtPlugin(async () => {
-  const authStore = useAuthStore()
-
-  // Uygulama başladığında auth durumunu kontrol et
-  const authCookie = useCookie('auth_token')
-  if (authCookie.value) {
-    authStore.setToken(authCookie.value)
-  }
-}) 
--- router.js
+++
@@ -1,2 +0,0 @@
-// Nuxt 3 automatically handles routing based on the pages directory structure
-// The login.vue file is now in the pages directory, so /login route will work automatically
