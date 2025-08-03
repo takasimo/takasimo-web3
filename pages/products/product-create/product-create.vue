9fc1b618bcdeaa3a583bd03e493fd044602f815e
otontas11 <oktay1125@gmail.com>
2025-08-03 10:51:13 +0300

    xxx

--- pages/products/product-create/product-create.vue
+++ pages/products/product-create/product-create.vue
@@ -1,9 +1,33 @@
 <script setup lang="ts">
+const route = useRoute()
 
+const categoryCode = computed(() => route.params?.code || '')
+
+const formData = ref<any>({
+  category_code: categoryCode.value,
+  name: null,
+  price: '',
+  quantity: 1,
+  swap: true,
+  description: null,
+  currency: 'TRY',
+  showcase_image: null,
+  accepted_communication_types: [],
+  city: null,
+  district: null,
+  localities: null,
+  full_address: null,
+  lat: 1,
+  lng: 1,
+  accepted_payment_types: ['PAYMENT_BY_HAND'],
+  condition:'new'
+})
 </script>
 
 <template>
+<div class="product-create-page">
 
+</div>
 </template>
 
 <style scoped>
