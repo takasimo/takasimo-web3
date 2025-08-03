ff34f3ba2e0316f6b4a31f5a9034c50c59b70f8b
otontas11 <oktay1125@gmail.com>
2025-08-03 11:53:25 +0300

    xxx

--- pages/products/product-create/product-create.vue
+++ pages/products/product-create/product-create.vue
@@ -1,7 +1,17 @@
+
+
+<template>
+<div class="product-create-page">
+
+</div>
+</template>
+
 <script setup lang="ts">
+import { useCategoriesApi } from '~/composables/api'
+
 const route = useRoute()
 
-const categoryCode = computed(() => route.params?.code || '')
+const categoryCode = computed(() => route.params?.id || '')
 
 const formData = ref<any>({
   category_code: categoryCode.value,
@@ -22,13 +32,25 @@ const formData = ref<any>({
   accepted_payment_types: ['PAYMENT_BY_HAND'],
   condition:'new'
 })
+
+const { getSubCategoriesById } = useCategoriesApi()
+
+onMounted(()=>{
+  initializeData()
+})
+
+
+const initializeData = async () => {
+  try {
+    // Load subcategories
+    const subCategoriesResponse = await getSubCategoriesById('eff195001c3925202d4c09d59cc458ae')
+    console.log("subCategoriesResponse",subCategoriesResponse)
+  } catch (error) {
+    console.error('Initialization error:', error)
+  }
+}
+
 </script>
-
-<template>
-<div class="product-create-page">
-
-</div>
-</template>
 
 <style scoped>
 
